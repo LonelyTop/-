0c66f2de5a74062ee6c4dfb728ca94fde0a8e203
LonelyTop <46240324+LonelyTop@users.noreply.github.com>
2019-06-23 22:55:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,21 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-const token = 'NTkyNTQ4NDMzNjU5NjkxMDA4.XRBMAQ.YrsX9hQhD77fMbth5pRlD79ai-A';
+const token = 'NTkyNTg4MTQ2ODM5OTEyNDQ4.XRBjog.1j2osXf143gyOP6SkHPBV6ALBYU';
 
-// This is what turns the bot on
 bot.on('ready', () => {
-    console.log('Bot successfully executed online.');
-    console.log('Any problems will show down below.');
-    bot.user.setActivity("Animal Jams Database", {type: "WATCHING"});
+    console.log('YouTube Bot successfully executed online.');
+
+    bot.user.setActivity('Lonely', 'https://twitch.tv/lonelytopyt');
 })
 
 
-// Commands
+
 bot.on('message', msg=>{
 
-    if(msg.content === "symbols"){
-        msg.channel.send("þ đ ÷ ∂");
-    }
-
-    if(msg.content === "cmds"){
-        msg.channel.send(sEmbed);
-    }
-
-    if(msg.content === "symbol"){
-        msg.channel.send('þ');
+    if(msg.content === "link"){
+        msg.channel.send("https://youtube.com/c/LonelyFN");
     }
 })
 
-
-// Embed template
-let newEmbed = new Discord.RichEmbed()
-.setColor("#ff00ba")
-.setTitle("Title")
-.setDescription(`Description here.`);
-
-// Cmds Embed
-let sEmbed = new Discord.RichEmbed()
-.setColor("#ff00ba")
-.setTitle("List of commands.")
-.setDescription(`Cmds - Shows list of available commands.
-Symbol - Sends an important symbol.
-Symbols - Sends all symbols.`);
-
-
-
-bot.login(process.env.BOT_TOKEN);
+bot.login(token);
